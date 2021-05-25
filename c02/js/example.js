8a0da3f3a8c0d3951c70bcc9a5ff777d24b23505
Shakeria Smith <49452991+ShakeriaCodes@users.noreply.github.com>
2021-05-25 13:45:14 -0800

    basic javascript exampld\e

---
+++ c02/js/example.js
@@ -0,0 +1,35 @@
+// Create variables for the welcome message
+var greeting = 'Howdy';
+var username = 'Molly';
+var message = '. please check your order:';
+
+// Concatenate the three variables above to create the welcome message
+var welcome = greeting + username + message;
+
+// Create variable to holdl details about the sign
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+// Get the element that has an id of greeting
+var el = document.getElementById('greeting');
+// Replace the content of that element with the personalized welcome message
+el.textContent = welcome;
+
+// Get the element that has an id of tiles the update its contents
+var elTiles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+// Get the element that has an id of subTotal then update its contents
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = ' $ ' + subTotal;
+
+// Get the element that has an id of shipping then update its contents
+var elShipping = document.getElementById('shipping');
+elShipping.textContent = ' $ ' + shipping;
+
+// Get the element that has and id of grandTotal then update its contents
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent = ' $ ' + grandTotal;
---
+++ c02/js/string-operator.js
@@ -0,0 +1,7 @@
+var greeting = 'Howdy';
+var username = 'Molly';
+
+var welcomeMessage = greeting + username + '!';
+
+var el = document.getElementById('greeting');
+el.textContent = welcomeMessage;
